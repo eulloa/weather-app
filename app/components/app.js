537ab8f8ce7d8e867090d496e68068d4d734d56d
Efren Ulloa <eulloa345@gmail.com>
2017-08-10 02:43:46 -0500

    Update main section functionality

--- app/components/app.js
+++ app/components/app.js
@@ -38,25 +38,45 @@ class App extends React.Component {
 	
 	render() {
         return (
-			<section>
+			<section className="weatherWidget">
 				<Input initialInputValue={this.state.submitValue} onClick={this.handleOnClick} onChange={this.handleOnChange} onKeyPress={this.handleKeyPress} />
 				{
 					this.state.weather.length > 0 && (
 						<ReactCSSTransitionGroup transitionName="anim" transitionAppear={true} transitionAppearTimeout={500} transitionEnter={false} transitionLeave={false}>
-							<section className="weatherWidget">
+							<section className="forecast">
 								<div className="mainSection">
 									<WeatherDisplay>
 										<h1>{this.state.city}</h1>
-										<h2>{WeatherAuxiliary.getDayAndTime()}</h2>
-										<h3>{this.state.weather[0].weather[0].description}</h3>
+										<h2>
+											{
+												this.state.shouldUpdateMainDisplay ?
+												WeatherAuxiliary.getDayOfWeek(this.state.updateData.dt, true) :
+												WeatherAuxiliary.getDayAndTime()
+											}
+										</h2>
+										<h3>
+											{
+												this.state.shouldUpdateMainDisplay ? 
+												this.state.updateData.weather[0].description :
+												this.state.weather[0].weather[0].description
+											}
+										</h3>
 										<section>
 											<img src={WeatherAuxiliary.getWeatherImage(this.state.weather[0].weather[0].icon)} alt={this.state.weather[0].weather[0].description} />
 											<div>
 												<span className={this.state.shouldHideF ? 'hidden' : ''}>
-													{Math.round(WeatherAuxiliary.kelvinToF(this.state.weather[0].temp.max))}
+													{
+														this.state.shouldUpdateMainDisplay ?
+														Math.round(WeatherAuxiliary.kelvinToF(this.state.updateData.temp.max)) :
+														Math.round(WeatherAuxiliary.kelvinToF(this.state.weather[0].temp.max))
+													}
 												</span>
 												<span className={this.state.shouldHideC ? 'hidden' : ''}>
-													{Math.round(WeatherAuxiliary.kelvinToC(this.state.weather[0].temp.max))}
+													{
+														this.state.shouldUpdateMainDisplay ?
+														Math.round(WeatherAuxiliary.kelvinToC(this.state.updateData.temp.max)) :
+														Math.round(WeatherAuxiliary.kelvinToC(this.state.weather[0].temp.max))
+													}
 												</span>
 												<button className={this.state.shouldHideF ? '' : 'inactive'} onClick={this.handleClickTemperature}>F</button>
 												<button className={this.state.shouldHideC ? '' : 'inactive'} onClick={this.handleClickTemperature}>C</button>
@@ -65,23 +85,23 @@ class App extends React.Component {
 									</WeatherDisplay>
 								</div>
 								<div className="forecastContainer">
-								{this.state.weather.map((data, i) => {
-									return <WeatherDisplay key={i}>
-												<div className="weatherDisplayer" onClick={() => {this.handleUpdateMainSection(data)}}>
-													<h1>{WeatherAuxiliary.getDayOfWeek(data.dt, false)}</h1>
-													<section>
-														<img src={WeatherAuxiliary.getWeatherImage(data.weather[0].icon)} alt={data.weather[0].description} />
-														<span className={this.state.shouldHideF ? 'hidden' : ''}>
-															{Math.round(WeatherAuxiliary.kelvinToF(data.temp.max)) + ' ' + Math.round(WeatherAuxiliary.kelvinToF(data.temp.min))}
-														</span>
-														<span className={this.state.shouldHideC ? 'hidden' : ''}>
-															{Math.round(WeatherAuxiliary.kelvinToC(data.temp.max)) + ' ' + Math.round(WeatherAuxiliary.kelvinToC(data.temp.max))}
-														</span>
-													</section>
-												</div>
-											</WeatherDisplay>
-								})}
-							</div>
+									{this.state.weather.map((data, i) => {
+										return <WeatherDisplay key={i}>
+													<div onClick={() => {this.handleUpdateMainSection(data)}}>
+														<h1>{WeatherAuxiliary.getDayOfWeek(data.dt, false)}</h1>
+														<section>
+															<img src={WeatherAuxiliary.getWeatherImage(data.weather[0].icon)} alt={data.weather[0].description} />
+															<span className={this.state.shouldHideF ? 'hidden' : ''}>
+																{Math.round(WeatherAuxiliary.kelvinToF(data.temp.max)) + ' ' + Math.round(WeatherAuxiliary.kelvinToF(data.temp.min))}
+															</span>
+															<span className={this.state.shouldHideC ? 'hidden' : ''}>
+																{Math.round(WeatherAuxiliary.kelvinToC(data.temp.max)) + ' ' + Math.round(WeatherAuxiliary.kelvinToC(data.temp.min))}
+															</span>
+														</section>
+													</div>
+												</WeatherDisplay>
+									})}
+								</div>
 							</section>
 						</ReactCSSTransitionGroup>
 					)
@@ -140,7 +160,6 @@ class App extends React.Component {
 	downloadWeather(city, apiKey) {
 		axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&APPID=' + apiKey)
 			.then((res) => {
-				console.log(res.data.list.slice(0, 5))
 				this.setState({
 					city: res.data.city.name,
 					weather: res.data.list.slice(0, 5)
