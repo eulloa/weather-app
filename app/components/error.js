6e4680c39343aa7950cbf5969f0e46d11bf4449c
Efren Ulloa <eulloa345@gmail.com>
2017-10-21 01:59:41 -0500

    Style tweak for Error component

--- app/components/error.js
+++ app/components/error.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import AttentionIcon from '../assets/attention.svg';
 
 export default Error = ({text}) => {
     return (
         <div className="errorContainer">
+            <img src={AttentionIcon} />
             <p>{text}</p>
         </div>
     );
